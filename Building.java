cea66a6f21a79e5105f67624d21a1085c57c890a
Priyanka-Mayara2003 <priyankamayara04@gmail.com>
2024-06-25 10:39:04 +0530

    Add files via upload

---
+++ Building.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Building
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+{
+int n=s.nextInt();
+int a[]=new int[n];
+int count=0;
+for(int i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+
+for(int i=0;i<n;i++)
+{
+if(a[0]<=a[i])
+{
+count++;
+}
+}
+System.out.println(count);
+}
+}
+}
+
+
+
+
+
+
+
+
+
